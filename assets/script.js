7d04bfa34bd5287b19e102c829467370b9243515
therealsweven <robssimpson11@gmail.com>
2023-02-22 11:44:37 -0700

    added search form, event listener, and table element code

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,28 @@
-var APIKey = "00c728b2ec9889aa99a1c18ebb53335d";
-// openweathermap.org API key
+// var APIKey = "00c728b2ec9889aa99a1c18ebb53335d";
+// // openweathermap.org API key
 
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+// var queryURL =
+//   "http://api.openweathermap.org/data/2.5/weather?q=" +
+//   city +
+//   "&appid=" +
+//   APIKey;
 // using for fetch/queries
 
-// func demo will remove**
-// function getApi() { 
-
+// // func demo will remove**
+// function getApi() {
 //   fetch(queryURL)
 //     .then(function (response) {
 //       return response.json();
 //     })
 //     .then(function (data) {
-//       console.log(data)
-
+//       console.log(data);
 
 //       //loop demo will remove**
 //       for (var i = 0; i < data.length; i++) {
 //         // Creating elements, tablerow, tabledata, and anchor
-//         var createTableRow = document.createElement('tr');
-//         var tableData = document.createElement('td');
-//         var link = document.createElement('a');
+//         var createTableRow = document.createElement("tr");
+//         var tableData = document.createElement("td");
+//         var link = document.createElement("a");
 
 //         // set demo will remove**
 //         link.textContent = data[i].html_url;
@@ -33,3 +35,55 @@ var APIKey = "00c728b2ec9889aa99a1c18ebb53335d";
 //       }
 //     });
 // }
+
+var city;
+var cityEl = $("#userCity");
+var WeatherEl = $("#weatherTable");
+
+//clear text input example on focus
+cityEl.on("focus", function () {
+  cityEl.val("");
+});
+
+//listen for city search
+$("#searchBtn").on("click", function () {
+  city = cityEl.val();
+  //   console.log(city);
+  if (localStorage.getItem("Search History") === null) {
+    localStorage.setItem("Search History", city);
+  } else {
+    console.log(localStorage.getItem("Search History"));
+  }
+  cityEl.val("");
+  //API key:  c553d4096a25b5d4d72caeabd2a72b94
+  requestUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=c553d4096a25b5d4d72caeabd2a72b94";
+  //   console.log(requestUrl);
+
+  fetch(requestUrl)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data.list);
+      //   for (var i = 0; i < data.length; i++) {
+      //     // Creating elements, tablerow, tabledata, and anchor
+      //     var createTableRow = document.createElement("tr");
+      //     var tableData = document.createElement("td");
+      //     var link = document.createElement("a");
+
+      //     // set demo will remove**
+      //     link.textContent = data[i].html_url;
+      //     link.href = data[i].html_url;
+
+      //     // append demo will remove**
+      //     tableData.appendChild(link);
+      //     createTableRow.appendChild(tableData);
+      //     tableBody.appendChild(createTableRow);
+      //   }
+    });
+});
