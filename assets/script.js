42922bd3f75147abc73bbe3a0799231b11828580
suturbreverof <sfriedman6711@gmail.com>
2023-02-22 10:55:59 -0700

    added queryurl

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,5 @@
 var APIKey = "00c728b2ec9889aa99a1c18ebb53335d";
 // openweathermap.org API key
+
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+// using for fetch/queries
